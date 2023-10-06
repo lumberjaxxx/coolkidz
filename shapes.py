3bad6593dd2bb3c730efabe4182eec0316d167a7
lumberjaxxx <143997567+lumberjaxxx@users.noreply.github.com>
2023-10-06 18:09:57 +0800

    added new subclass

--- shapes.py
+++ shapes.py
@@ -44,11 +44,30 @@ class Rectangle(Shape):
         print(f'The area of rectangle is {Rectangle.area(self)}')
         print(f'The perimeter of rectangle is {Rectangle.perimeter(self)}')
 
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def area(self):
+        x = 3.14 * (self.radius**2)
+        return x
+    
+    def perimeter(self):
+        p = 2 * 3.14 * self.radius
+        return p
+
+    def display(self):
+        print(f'The area of rectangle is {Circle.area(self)}')
+        print(f'The perimeter of rectangle is {Circle.perimeter(self)}')
+
 
 rectangle = Rectangle(3,4)
 sqre = Square(5)
+circle = Circle(7)
 
 rectangle.display()
 sqre.display()
+circle.display()
+
 
 
