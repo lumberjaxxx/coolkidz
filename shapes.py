27939f37129e47579249f05c535e44afdbf09106
lumberjaxxx <143997567+lumberjaxxx@users.noreply.github.com>
2023-10-06 18:00:17 +0800

    Add files via upload

---
+++ shapes.py
@@ -0,0 +1,54 @@
+class Shape:
+    def __init__(self):
+        self.units = "meters"
+
+    def area(self):
+        pass
+
+    def perimeter(self):
+        pass
+
+    def display(self):
+        pass
+
+class Square(Shape):
+    def __init__(self, sides):
+        self.sides=sides
+
+    def area(self):
+        x = self.sides**2
+        return x
+
+    def perimeter(self):
+        p = 4 * self.sides
+        return p
+        
+    def display(self):
+        print(f'The area of square is {Square.area(self)}')
+        print(f'The perimeter of square is {Square.perimeter(self)}')
+        
+class Rectangle(Shape):
+    def __init__(self, l, w):
+        self.l = l
+        self.w = w
+    
+    def area(self):
+        x = self.l * self.w
+        return x
+    
+    def perimeter(self):
+        p = 2 * (self.l + self.w)
+        return p
+
+    def display(self):
+        print(f'The area of rectangle is {Rectangle.area(self)}')
+        print(f'The perimeter of rectangle is {Rectangle.perimeter(self)}')
+
+
+rectangle = Rectangle(3,4)
+sqre = Square(5)
+
+rectangle.display()
+sqre.display()
+
+
